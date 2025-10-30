40439b1c21567fd157cb66e7d3e705a75bfec6af
Mark1q <mark.eduard7@gmail.com>
2025-10-30 21:17:19 +0200

    Implement JWT-based authentication routes

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser } = require('./../controller/auth.controller')
+const { registerUser, loginUser, refreshToken, logoutUser } = require('./../controller/auth.controller')
 
-router.post('/', registerUser)
-router.post('/login', loginUser)
+router.post('/', registerUser);
+router.post('/login', loginUser);
+router.post('/logout', logoutUser);
+router.post('/refresh', refreshToken);
 
 module.exports = router;
