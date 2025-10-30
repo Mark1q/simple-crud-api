9bce4a2ccf4e5db39c783f35d938f8fa45a56876
Mark1q <mark.eduard7@gmail.com>
2025-10-30 21:19:07 +0200

    Add role-based access control for product CRUD routes

--- routes/product.route.js
+++ routes/product.route.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const { getProducts, getProduct, createProduct, updateProduct, deleteProduct } = require('../controller/product.controller.js');
-const { auth } = require('./../middlware/auth.js')
+const { auth } = require('../middlware/authentication.js');
+const { authorize } = require('../middlware/authorize.js');
 
 router.get('/', getProducts);
 router.get('/:id', getProduct);
 
-router.post('/', auth, createProduct)
+router.post('/', auth, authorize('admin'), createProduct);
 
 // update a product
 
-router.put('/:id', auth, updateProduct);
+router.put('/:id', auth, authorize('user'), updateProduct);
 
 // delete a product
 
-router.delete('/:id', auth, deleteProduct);
+router.delete('/:id', auth, authorize('admin'), deleteProduct);
 
 module.exports = router;
