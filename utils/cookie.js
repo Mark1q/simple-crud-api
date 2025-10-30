d455a0fdd47c99a61a5c7383ee8ec9171fb5a7dc
Mark1q <mark.eduard7@gmail.com>
2025-10-30 21:20:57 +0200

    Create constants module for application-wide values

---
+++ utils/cookie.js
@@ -0,0 +1,3 @@
+module.exports = {
+    COOKIE_EXPIRES_MS: 7 * 24 * 60 * 60 * 1000 // in miliseconds
+};
---
+++ utils/tokens.js
@@ -0,0 +1,4 @@
+module.exports = {
+    ACCESS_TOKEN_EXPIRES: '30m',
+    REFRESH_TOKEN_EXPIRES: '7d'
+};
