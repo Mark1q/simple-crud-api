d4b227235408bda3994622b68db6ab5f83d45bfb
Mark1q <mark.eduard7@gmail.com>
2025-10-31 17:48:04 +0200

    Removed redundant password from .env configuration

--- config.js
+++ config.js
@@ -4,7 +4,6 @@ const { cleanEnv, str, port } = require("envalid");
 const env = cleanEnv(process.env, {
   PORT: port({ default: 5000 }),
   MONGO_URI: str(),
-  MONGODB_PASSWORD: str(),
   ACCESS_TOKEN_SECRET: str(),
   REFRESH_TOKEN_SECRET: str(),
   NODE_ENV: str({default: 'development'})
