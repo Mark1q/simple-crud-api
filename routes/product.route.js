70d0e28dcc761f353c1322148fcbad67dbf66e19
Mark1q <mark.eduard7@gmail.com>
2025-11-02 10:38:46 +0200

    Fix middleware import typo and add product creation validation

--- routes/product.route.js
+++ routes/product.route.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const { getProducts, getProduct, createProduct, updateProduct, deleteProduct } = require('../controller/product.controller.js');
-const { auth } = require('../middlware/authentication.js');
-const { authorize } = require('../middlware/authorize.js');
+const { auth } = require('../middleware/authentication.js');
+const { authorize } = require('../middleware/authorize.js');
+const { validate } = require('./../middleware/validate.js');
+const { createProductSchema } = require('./../validations/product.validation.js');
+
 
 router.get('/', getProducts);
 router.get('/:id', getProduct);
 
-router.post('/', auth, authorize('admin'), createProduct);
+router.post('/', auth, authorize('admin'), validate(createProductSchema),  createProduct);
 
 // update a product
 
