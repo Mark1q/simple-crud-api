bd9535e283233e6036d3863a522eed2a49634899
Mark1q <mark.eduard7@gmail.com>
2025-11-02 21:35:17 +0200

    Implement Joi schema for updating product

--- validations/product.validation.js
+++ validations/product.validation.js
@@ -5,8 +5,16 @@ const createProductSchema = Joi.object({
   price: Joi.number().positive().required(),
   quantity: Joi.number().integer().min(0).default(0),
   image: Joi.string().uri().optional()
-});
+}).min(1);
+
+const updateProductSchema = Joi.object({
+  name: Joi.string().min(3).max(100).optional(),
+  price: Joi.number().positive().optional(),
+  quantity: Joi.number().integer().min(0).optional(),
+  image: Joi.string().uri().optional()
+}).min(1);
 
 module.exports = {
-  createProductSchema
+  createProductSchema,
+  updateProductSchema
 }
