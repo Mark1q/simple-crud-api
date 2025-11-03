ada4c52b79b4719eaf1c8660697b0468fd5130ba
Mark1q <mark.eduard7@gmail.com>
2025-11-03 22:28:47 +0200

    Change name to fit naming convention

---
+++ middleware/authenticate.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken')
+const config = require('../config.js')
+
+const auth = async (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.status(401).json({ error: 'No token provided' });
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, config.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).json({ error: 'Invalid or expired token' });
+        }
+
+        req.userId = decoded.userId; // attach user ID for controllers to use
+        req.role = decoded.role; // attach user Role 
+        next();
+    });
+}
+
+module.exports = { auth }
